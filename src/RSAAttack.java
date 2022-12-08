1cf18e4aa16e1f633b4d6c4e61c8dd2b7355a085
Douglas <douglaspereira04@hotmail.com>
2022-12-08 16:07:26 -0300

    adds wieners algorithm signature

--- src/RSAAttack.java
+++ src/RSAAttack.java
@@ -1,4 +1,13 @@
-
+import java.math.BigInteger;
 public class RSAAttack {
 
+	/**
+	 * Wiener's Algorithm
+	 * @param n
+	 * @param b
+	 * @return one factor p or failure
+	 */
+	public static BigInteger wienersAttack(BigInteger n, BigInteger b) {
+		return null;
+	}
 }
