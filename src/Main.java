0ebb4c25f10c4ccdfe7c7bb104d1ea110a5fa5ab
Douglas <douglaspereira04@hotmail.com>
2022-12-09 16:46:11 -0300

    make pollar p - 1 test parallel

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		testPollardPMinus1(200, 4, b -> b.sqrt().sqrt());
-		testPollardPMinus1(200, 8, b -> b.sqrt().sqrt());
-		testPollardPMinus1(200, 16, b -> b.sqrt().sqrt().sqrt());
+		testPollardPMinus1(200, 4, b -> b.sqrt(),4);
+		testPollardPMinus1(200, 8, b -> b.sqrt().sqrt(),4);
+		testPollardPMinus1(200, 16, b -> b.sqrt().sqrt().sqrt(),4);
 		
 
 		Function<BigInteger, BigInteger> f = x -> x.pow(x.intValue()).add(BigInteger.valueOf(1));
@@ -21,35 +22,66 @@ public class Main {
 	}
 	
 	
-	public static long testPollardPMinus1(int tests, int nSize, Function<BigInteger, BigInteger> bcalc) {
+	public static long testPollardPMinus1(int tests, int nSize, Function<BigInteger, BigInteger> bcalc, int threadAmount) throws InterruptedException {
 
 		System.out.println("Pollard p-1: (tests = "+tests+", size =~ "+nSize*8+")");
-		BigInteger n = null;
-		BigInteger B = null;
 
-		BigInteger p;
-		int randomHits = 0, strongHits = 0;
+		AtomicInteger randomHits = new AtomicInteger(0), strongHits = new AtomicInteger(0);
+		long begin = System.nanoTime();
+		Thread[] threads = new Thread[threadAmount];
+
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(()->{
+				BigInteger p;
+				BigInteger n = null;
+				BigInteger B = null;
+				
+				for (int j = 0; j < tests/threadAmount; j++) {
+					n = IntegerFactorizer.pollardPMinusOneStrong(nSize, 8);
+					B = bcalc.apply(n);
+					p = IntegerFactorizer.pollardPMinusOne(n, B, 8);
+					if(p != null) {
+						strongHits.incrementAndGet();
+					}
+					n = IntegerFactorizer.randomN(nSize, nSize/2, 8);
+					B = bcalc.apply(n);
+					p = IntegerFactorizer.pollardPMinusOne(n, B, 8);
+					if(p != null) {
+						randomHits.incrementAndGet();
+					}
+				}
+			});
+			threads[i].start();
+		}
+		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].join();
+		}
+		long elapsedTime = System.nanoTime() - begin;
+		System.out.println("\tElapsed time: "+elapsedTime+"ns");
+		System.out.println("\tRandom n success ratio: "+randomHits.get()*100.0/tests+"%");
+		System.out.println("\tStrong n success ratio: "+strongHits.get()*100.0/tests+"%");
+		
+		return elapsedTime;
+	}
+	
+	public static long testWieners(int tests, int nSize) {
+		System.out.println("Wieners: (tests = "+tests+", size =~ "+nSize*8+")");
+
+		int randomHits = 0, weakHits = 0;
+		
 		long begin = System.nanoTime();
 		for (int i = 0; i < tests; i++) {
-			n = IntegerFactorizer.pollardPMinusOneStrong(nSize, 8);
-			B = bcalc.apply(n);
-			p = IntegerFactorizer.pollardPMinusOne(n, B, 8);
-			if(p != null) {
-				strongHits++;
-			}
-			n = IntegerFactorizer.randomN(nSize, nSize/2, 8);
-			B = bcalc.apply(n);
-			p = IntegerFactorizer.pollardPMinusOne(n, B, 8);
-			if(p != null) {
-				randomHits++;
-			}
+			
 		}
 		long elapsedTime = System.nanoTime() - begin;
 		System.out.println("\tElapsed time: "+elapsedTime+"ns");
 		System.out.println("\tRandom n success ratio: "+randomHits*100.0/tests+"%");
-		System.out.println("\tStrong n success ratio: "+strongHits*100.0/tests+"%");
+		System.out.println("\tWeak n success ratio: "+weakHits*100.0/tests+"%");
 		
 		return elapsedTime;
+		
+		
 	}
 	
 }
